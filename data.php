f2ae94ad39c44c64c48e8737c2ab1ecba5fb1c0c
Maha <proma.1487@gmail.com>
2019-05-18 06:26:16 +0300

    First Login page

--- data.php
+++ data.php
@@ -9,8 +9,7 @@ $db_host = getenv('DB_host'); //استدعاء البيانات من الملف 
 $db_username = getenv('DB_username');
 $db_password = getenv('DB_password');
 $Database = getenv('DB');
-$db_adminuser=getenv('DB_adminname');
-$db_adminpassword=getenv('DB_adminpassword');
+
 // echo $db_host;
 // echo $db_username;
 // echo $Database;
@@ -19,24 +18,22 @@ $Adminpassword = $_POST['passwordadmin'];
 $connection = mysqli_connect($db_host, $db_username, $db_password, $Database, '8889'); //الاتصال بقاعدة
 $Username = mysqli_real_escape_string($connection, $Adminusername);
 $Password = mysqli_real_escape_string($connection, $Adminpassword);
-//التحقق من ان كلمة المرور واسم المستخدم ليست فارغه
-if (isset($Adminusername)&&!empty($Adminusername)) {
-    if (isset($Adminpassword)&&!empty($Adminpassword)) {
-    
-} 
-}
+
 //الاستعلام عن اسم المستخدم وكلمة المرور
-$query=mysqli_query($connection,"select * from Admin where $db_adminuser='$Adminusername' AND $db_adminpassword='$Adminpassword'")
-    or die("failed to query database " . mysqli_error($connection));
+$query = mysqli_query($connection, "select * from Admin where Username='$Adminusername'")
+or die("failed to query database " . mysqli_error($connection));
 $result = mysqli_fetch_array($query);
+$db_adminpassword = $result['Password'];
+
 //التحقق من ان اسم المستخدم وكلمة المرور موجوده بقاعدة البيانات
-if ($result > 0) {
+if ($result > 0 && password_verify($Adminpassword, $db_adminpassword)) {
     echo "Welcome Admin";
 } else {
+    
+    //echo"<script> alert('username or password is Error ')</script>";
      header('Location: ' . $_SERVER['HTTP_REFERER']);
-}
-
+} 
 ?>
-<form method="Post"action="logout.php">
-<input type="submit"value="Logout">
+<form method="Post" action="logout.php">
+<input type="submit" value="Logout">
 </form>
